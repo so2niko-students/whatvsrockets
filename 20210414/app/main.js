9a62e9c20b9b87a62770e279f1b5a84ee389136f
Nick Sotula <so2niko@gmail.com>
2021-04-14 17:06:47 +0300

    docs: let, const, var, for

---
+++ 20210414/app/main.js
@@ -0,0 +1,24 @@
+// console.log(b);
+
+var a = 1;
+let b = 1;
+const c = 1; //Константа
+
+let j = 0;
+while(j < 10){
+    //что-то делаю
+
+    j++;
+}
+
+for(let i = 1, jj = 3, abrakadfdf = 33; i < 10; i += 4){
+    console.log(i);
+}
+
+// console.log(i);
+
+const nums = [1, 2, 3, 4];
+
+nums.push(5);
+
+console.log(nums);
