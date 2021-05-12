d9974c766e4e1de8058aab57ffd3bd68d23866e0
Nick Sotula <so2niko@gmail.com>
2021-05-12 16:40:51 +0300

    feat: add Modal

--- total_recall/app/change/viewChange.js
+++ total_recall/app/change/viewChange.js
@@ -1,29 +1,56 @@
 export default class ViewChange{
+    modalClassToggle = 'is-active';
+    modalFreezeBack = 'is-clipped';
+
     constructor(handleSubmitChangeName){
         this.DOMFormWrapper = document.querySelector('.form_change');
+        this.DOMModal = document.querySelector('.modal');
+
         this.handleSubmitChangeName = handleSubmitChangeName;
     }
 
     render(date){
         this.DOMFormWrapper.innerHTML = this.renderForm(date);
+        this.addListeners();
+        this.DOMModal.classList.add(this.modalClassToggle);
+        document.documentElement.classList.add(this.modalFreezeBack);
+    }
+
+    addListeners(){
         document.querySelector('.btn_submit_change_name').addEventListener('click', this.handleSubmitChangeName);
+        [...document.querySelectorAll('.btn_close_modal_form')].forEach(el => el.addEventListener('click', this.closeModal));
     }
 
     renderForm({ name }){
-        return `<div class="field">
-            <label class="label" for="inp_name">Name</label>
-            <div class="control">
-            <input class="input" type="text" 
-                placeholder="No one is chosen" 
-                id="inp_name" value="${ name }">
-            </div>
-            <button 
-                class="button is-danger is-light btn_submit_change_name">Submit</button>
-            </div>`;
+        return `
+            <header class="modal-card-head">
+                <p class="modal-card-title">Change Name</p>
+                <button class="delete btn_close_modal_form" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+                <div class="field">
+                    <label class="label" for="inp_name">Name</label>
+                    <div class="control">
+                        <input class="input" type="text" 
+                        placeholder="No one is chosen" 
+                        id="inp_name" value="${ name }">
+                    </div>
+                </div>
+            </section>
+            <footer class="modal-card-foot">
+                <button 
+                    class="button is-danger is-light btn_submit_change_name">Submit</button>
+            </footer>
+            `;
     }
 
     getName(){
         return document.querySelector('#inp_name').value;
+    }
+
+    closeModal = () => {
+        this.DOMModal.classList.remove(this.modalClassToggle);
+        document.documentElement.classList.remove(this.modalFreezeBack);
     }
 
 }
